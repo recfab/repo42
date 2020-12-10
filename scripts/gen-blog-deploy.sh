5179d9c75ce3923f3fc23da3b8dae1ea1ee44550
Yael Namen <yael.namen@icloud.com>
2020-12-10 22:54:01 +0000

    blog: Create initial CI/CD
    There are some things I still need to do:
    
    * There isn't an Ingress, yet.
    * The deployed service shows the default nginx index.html
    
    However, this is a good checkpoint so I am going to merge it now and work on those as separate issues.

---
+++ scripts/gen-blog-deploy.sh
@@ -0,0 +1,49 @@
+#! /usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+thisDir=$(dirname "$0")
+
+# shellcheck source=scripts/variable-helpers.sh
+. "$thisDir/variable-helpers.sh"
+
+if [ -z ${CI+x} ]; then
+    echo "💡 Because you are not running in a CI/CD pipeline, I presume that you are developing this script. I will set some variables for you to make that easier."
+    echo
+
+    set_common_ci_vars
+    set_env_ci_vars
+fi
+
+print_project_ci_vars
+print_commit_ci_vars
+print_registry_ci_vars
+print_environment_ci_vars
+
+K8S_DIR="$CI_PROJECT_DIR/k8s"
+BUILD_DIR="$CI_PROJECT_DIR/build"
+
+GEN_DIR="$BUILD_DIR/kustomize"
+mkdir -p "$GEN_DIR"
+
+GEN_FILE="$GEN_DIR/kustomization.yaml"
+
+rm -f "$GEN_FILE"
+touch "$GEN_FILE"
+
+BASE="$K8S_DIR"
+
+REL_BASE="$(realpath "$BASE" -m --relative-to="$GEN_DIR")"
+
+print_ci_vars_by_prefix '(K8S_DIR|BUILD_DIR|BASE|REL_BASE|GEN_)' "My vars"
+
+yq w -i "$GEN_FILE" 'bases[+]' "$REL_BASE"
+
+yq w -i "$GEN_FILE" 'commonAnnotations[app.kubernetes.io/version]' "$CI_COMMIT_SHORT_SHA"
+yq w -i "$GEN_FILE" 'commonAnnotations[app.gitlab.com/env]' "$CI_ENVIRONMENT_NAME"
+yq w -i "$GEN_FILE" 'commonAnnotations[app.gitlab.com/app]' "$CI_PROJECT_PATH_SLUG"
+
+yq w -i "$GEN_FILE" 'images[+].name' 'blog'
+yq w -i "$GEN_FILE" 'images(name==blog).newName' "$CI_REGISTRY_IMAGE/blog"
+yq w -i "$GEN_FILE" 'images(name==blog).newTag' "$CI_COMMIT_REF_SLUG"
---
+++ scripts/variable-helpers.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/env bash
+
+set_common_ci_vars() {
+    remote=$(git remote get-url origin)
+
+    CI_PROJECT_NAME=$(echo "$remote" | awk -F '[/.]' '{ print $(NF-1) }')
+    CI_PROJECT_NAMESPACE=$(echo "$remote" | awk -F '[/.]' '{ print $(NF-2) }')
+    CI_PROJECT_DIR=$(git rev-parse --show-toplevel)
+    CI_PROJECT_PATH='recfab/main'
+    CI_PROJECT_PATH_SLUG="${CI_PROJECT_PATH//[^a-z09]/-}"
+
+    CI_COMMIT_REF_NAME=$(git branch --show-current)
+    CI_COMMIT_REF_SLUG="${CI_COMMIT_REF_NAME//[^a-z09]/-}"
+    CI_COMMIT_SHA=$(git rev-parse HEAD)
+    CI_COMMIT_SHORT_SHA=$(git rev-parse --short HEAD)
+    CI_COMMIT_BEFORE_SHA=$(git rev-parse @~)
+    CI_COMMIT_DESCRIPTION=$(git log -1 --pretty=%s)
+
+    CI_REGISTRY="http://registry.example.com"
+    CI_REGISTRY_IMAGE="$CI_REGISTRY/$CI_PROJECT_PATH"
+
+    export CI_PROJECT_NAME
+    export CI_PROJECT_NAMESPACE
+    export CI_PROJECT_DIR
+    export CI_PROJECT_PATH
+    export CI_PROJECT_PATH_SLUG
+
+    export CI_COMMIT_REF_NAME
+    export CI_COMMIT_REF_SLUG
+    export CI_COMMIT_SHA
+    export CI_COMMIT_SHORT_SHA
+    export CI_COMMIT_BEFORE_SHA
+    export CI_COMMIT_DESCRIPTION
+
+    export CI_REGISTRY
+    export CI_REGISTRY_IMAGE
+}
+
+set_env_ci_vars() {
+    CI_ENVIRONMENT_NAME="review/$CI_COMMIT_REF_SLUG"
+    CI_ENVIRONMENT_SLUG="${CI_ENVIRONMENT_NAME//[^a-z09]/-}"
+    CI_ENVIRONMENT_URL="https://example.com/$CI_COMMIT_REF_SLUG"
+
+    export CI_ENVIRONMENT_NAME
+    export CI_ENVIRONMENT_SLUG
+    export CI_ENVIRONMENT_URL
+}
+
+print_ci_vars_by_prefix() {
+    prefix="$1"
+    title="$2"
+
+    printf "\e[4m%s\e[0m\n" "$title"
+    set | grep -E -e "^$prefix.*=" |
+        awk -F '[=]' '{ printf "\033[2m%-30s\033[0m %s\n", $1, $2 }'
+    echo
+}
+
+print_project_ci_vars() {
+    print_ci_vars_by_prefix 'CI_PROJECT' 'GitLab predefined variables: Project'
+}
+
+print_commit_ci_vars() {
+    print_ci_vars_by_prefix 'CI_COMMIT' 'GitLab predefined variables: Commit'
+}
+
+print_registry_ci_vars() {
+    print_ci_vars_by_prefix 'CI_REGISTRY' 'Gitlab predefined variables: Registry'
+}
+
+print_environment_ci_vars() {
+    print_ci_vars_by_prefix 'CI_ENVIRONMENT_' 'GitLab predefined variables: Environment'
+}
