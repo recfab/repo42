34abb7140a8ef5b0b80f7684477cf84ca43a0eae
Yael Namen <yael.namen@icloud.com>
2021-04-15 10:44:50 -0700

    Rewrite tf-init-local script to use envvars.

--- scripts/tf-init-local.sh
+++ scripts/tf-init-local.sh
@@ -1,24 +1,23 @@
 #! /usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
 
 thisDir=$(dirname "$0")
 terraformDir=$(realpath "$thisDir/../terraform")
 
-PROJECT_ID='21637940'
-TF_STATE_NAME='default'
-TF_USERNAME='recfab'
-TF_PASSWORD="$GITLAB_ACCESS_TOKEN"
-TF_ADDRESS="https://gitlab.com/api/v4/projects/${PROJECT_ID}/terraform/state/$TF_STATE_NAME"
+tf_state_name="$CI_COMMIT_REF_SLUG"
+export TF_HTTP_ADDRESS="https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/terraform/state/$tf_state_name"
+
+export TF_HTTP_USERNAME="$CI_DEPLOY_USER"
+export TF_HTTP_PASSWORD="$CI_DEPLOY_PASSWORD"
+
+export TF_HTTP_LOCK_ADDRESS="$TF_HTTP_ADDRESS/lock"
+export TF_HTTP_UNLOCK_ADDRESS="$TF_HTTP_ADDRESS/lock"
+export TF_HTTP_LOCK_METHOD='POST'
+export TF_HTTP_UNLOCK_METHOD='DELETE'
 
 pushd "$terraformDir" || exit
 
-terraform init \
-    -backend-config=address=${TF_ADDRESS} \
-    -backend-config=lock_address=${TF_ADDRESS}/lock \
-    -backend-config=unlock_address=${TF_ADDRESS}/lock \
-    -backend-config=username=${TF_USERNAME} \
-    -backend-config=password="${TF_PASSWORD}" \
-    -backend-config=lock_method=POST \
-    -backend-config=unlock_method=DELETE \
-    -backend-config=retry_wait_min=5
+terraform init
 
 popd || exit
