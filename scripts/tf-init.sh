f5bf99c37538e3a94543031a954fe7a45cf3f4f2
Yael Namen <yael.namen@icloud.com>
2021-07-04 23:20:30 -0700

    Factor out a common .terraform base job

--- scripts/tf-init.sh
+++ scripts/tf-init.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-tf_state_name="$CI_COMMIT_REF_SLUG"
+tf_state_name="main"
 export TF_HTTP_ADDRESS="https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/terraform/state/$tf_state_name"
 
-export TF_HTTP_USERNAME="$CI_DEPLOY_USER"
-export TF_HTTP_PASSWORD="$CI_DEPLOY_PASSWORD"
+export TF_HTTP_USERNAME="recfab"
+export TF_HTTP_PASSWORD="$GITLAB_TOKEN"
 
 export TF_HTTP_LOCK_ADDRESS="$TF_HTTP_ADDRESS/lock"
 export TF_HTTP_UNLOCK_ADDRESS="$TF_HTTP_ADDRESS/lock"
