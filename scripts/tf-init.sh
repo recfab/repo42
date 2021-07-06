b71e3cfb432464ef1098bcf73aba103b9fa31fca
Yael Namen <yael.namen@icloud.com>
2021-07-06 10:49:47 -0700

    Tweak tf-init.sh

--- scripts/tf-init.sh
+++ scripts/tf-init.sh
@@ -2,11 +2,19 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+export CI_PROJECT_ID=${CI_PROJECT_ID:-21637940}
+export CI_COMMIT_REF_SLUG=${CI_COMMIT_REF_SLUG:-main}
+
 tf_state_name="main"
 export TF_HTTP_ADDRESS="https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/terraform/state/$tf_state_name"
 
-export TF_HTTP_USERNAME="recfab"
-export TF_HTTP_PASSWORD="$GITLAB_TOKEN"
+if [[ -z ${CI_JOB_TOKEN:+x} ]]; then
+  export TF_HTTP_USERNAME="recfab"
+  export TF_HTTP_PASSWORD="$GITLAB_TOKEN"
+else
+  export TF_HTTP_USERNAME="gitlab-ci-token"
+  export TF_HTTP_PASSWORD="$CI_JOB_TOKEN"
+fi
 
 export TF_HTTP_LOCK_ADDRESS="$TF_HTTP_ADDRESS/lock"
 export TF_HTTP_UNLOCK_ADDRESS="$TF_HTTP_ADDRESS/lock"
