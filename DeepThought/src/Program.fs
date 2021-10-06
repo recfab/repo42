27ebeb477c17407addf9f394fe1ccc24270fd6be
Yael Namen <yael.namen@icloud.com>
2021-10-05 20:38:15 -0700

    Stub args for DeepThought

--- DeepThought/src/Program.fs
+++ DeepThought/src/Program.fs
@@ -1,13 +1,25 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
+open Argu
 open System
 
+type Args =
+  | Ls
+  | NewNote
+  interface IArgParserTemplate with
+    member this.Usage =
+      match this with
+      | Ls -> "list notes"
+      | NewNote -> "create a new note and opens it in $EDITOR"
+
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let from whom = sprintf "from %s" whom
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+  let parser =
+    ArgumentParser.Create<Args>(programName = "dat")
+
+  let usage = parser.PrintUsage()
+  printf "%s" usage
+  0 // return an integer exit code
